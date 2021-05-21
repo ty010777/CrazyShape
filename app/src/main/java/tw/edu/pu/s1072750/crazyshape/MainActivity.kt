014050d59ddeec4d5c8caf3af82e0afccd3b8ae3
Jerry <ty010777@gmail.com>
2021-05-21 14:13:30 +0800

    2021上機考v2_(陳嘉良)

--- app/src/main/java/tw/edu/pu/s1072750/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1072750/crazyshape/MainActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +27,11 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+        val img: ImageView = findViewById(R.id.imgTitle)
+
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .into(img)
 
     }
 }
