ca9acc8c5420c4934ef0a554dafb959aebdf472f
Jerry <ty010777@gmail.com>
2021-05-21 10:57:02 +0800

    2021上機考v0_(陳嘉良)

--- app/src/main/java/tw/edu/pu/s1072750/crazyshape/GameActivity.kt
+++ app/src/main/java/tw/edu/pu/s1072750/crazyshape/GameActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
